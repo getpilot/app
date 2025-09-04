62182b0cf9af97f93b03631014e4ca10b6df6040
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2025-09-04 18:46:53 +0530

    fix lint error

--- src/app/api/webhooks/instagram/route.ts
+++ src/app/api/webhooks/instagram/route.ts
@@ -82,7 +82,7 @@ export async function POST(request: Request) {
 
     if (igUserId && senderId && hasMessage) {
       console.log("Looking up integration for igUserId", igUserId);
-      let integration = await db.query.instagramIntegration.findFirst({
+      const integration = await db.query.instagramIntegration.findFirst({
         where: eq(instagramIntegration.instagramUserId, igUserId),
       });
       console.log("Integration found by igUserId?", Boolean(integration));
