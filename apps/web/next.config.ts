c3397f6ba8c25d28dd651fade5c57c32447d81f6
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2026-02-27 01:21:05 +0530

    add website

---
+++ apps/web/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  cacheComponents: true,
+
+  reactCompiler: true,
+
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+};
+
+export default nextConfig;
